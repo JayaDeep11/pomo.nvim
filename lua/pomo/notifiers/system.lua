f2a0ccd87510ea04873d02e5de5dfdeaaf9288f2
jayadeep11 <jaydeep.bellamkonda@gmail.com>
2024-02-13 23:05:08 +0530

    changed to Drawin to Linux

--- lua/pomo/notifiers/system.lua
+++ lua/pomo/notifiers/system.lua
@@ -1,5 +1,5 @@
-local log = require "pomo.log"
-local util = require "pomo.util"
+local log = require("pomo.log")
+local util = require("pomo.util")
 
 ---A `pomo.Notifier` that sends a system notification when the timer is finished.
 ---@class pomo.SystemNotifier : pomo.Notifier
@@ -8,21 +8,21 @@ local util = require "pomo.util"
 ---@field opts table
 local SystemNotifier = {}
 
-SystemNotifier.supported_oss = { util.OS.Darwin }
+SystemNotifier.supported_oss = { util.OS.Linux }
 
 ---@param timer pomo.Timer
 ---@param opts table|?
 ---@return pomo.SystemNotifier
 SystemNotifier.new = function(timer, opts)
-  if not vim.tbl_contains(SystemNotifier.supported_oss, util.get_os()) then
-    error(string.format("SystemNotifier is not implemented for your OS (%s)", util.get_os()))
-  end
+	if not vim.tbl_contains(SystemNotifier.supported_oss, util.get_os()) then
+		error(string.format("SystemNotifier is not implemented for your OS (%s)", util.get_os()))
+	end
 
-  local self = setmetatable({}, { __index = SystemNotifier })
-  self.timer = timer
-  self.notification = nil
-  self.opts = opts and opts or {}
-  return self
+	local self = setmetatable({}, { __index = SystemNotifier })
+	self.timer = timer
+	self.notification = nil
+	self.opts = opts and opts or {}
+	return self
 end
 
 ---@param time_left number
@@ -33,23 +33,23 @@ SystemNotifier.start = function(self) ---@diagnostic disable-line: unused-local
 end
 
 SystemNotifier.done = function(self) ---@diagnostic disable-line: unused-local
-  local repetitions_str = ""
-  if self.timer.max_repetitions ~= nil and self.timer.max_repetitions > 0 then
-    repetitions_str = string.format(" [%d/%d]", self.timer.repetitions + 1, self.timer.max_repetitions)
-  end
+	local repetitions_str = ""
+	if self.timer.max_repetitions ~= nil and self.timer.max_repetitions > 0 then
+		repetitions_str = string.format(" [%d/%d]", self.timer.repetitions + 1, self.timer.max_repetitions)
+	end
 
-  if util.get_os() == util.OS.Darwin then
-    os.execute(
-      string.format(
-        [[osascript -e 'display notification "Timer done!" with title "Timer #%d, %s%s" sound name "Ping"']],
-        self.timer.id,
-        util.format_time(self.timer.time_limit),
-        repetitions_str
-      )
-    )
-  else
-    return log.error("SystemNotifier is not implemented for your OS (%s)", util.get_os())
-  end
+	if util.get_os() == util.OS.Linux then
+		os.execute(
+			string.format(
+				[[osascript -e 'display notification "Timer done!" with title "Timer #%d, %s%s" sound name "Ping"']],
+				self.timer.id,
+				util.format_time(self.timer.time_limit),
+				repetitions_str
+			)
+		)
+	else
+		return log.error("SystemNotifier is not implemented for your OS (%s)", util.get_os())
+	end
 end
 
 SystemNotifier.stop = function(self) ---@diagnostic disable-line: unused-local
